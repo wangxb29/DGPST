fccc08a3b488470232aa78de8fa5d3c1e6275900
wangxb29 <148447509+wangxb29@users.noreply.github.com>
2025-09-11 13:19:59 +0800

    Delete faceutils directory

--- faceutils/__init__.py
+++
@@ -1,5 +0,0 @@
-#!/usr/bin/python
-# -*- encoding: utf-8 -*-
-from . import faceplusplus as fpp
-from . import dlibutils as dlib
-from . import mask
--- faceutils/dlibutils/__init__.py
+++
@@ -1,3 +0,0 @@
-#!/usr/bin/python
-# -*- encoding: utf-8 -*-
-from .main import detect, crop, landmarks, crop_from_array
